db3e604056e3e1ff03e87b6dd066b92dc7b0b523
the51craw <hakan.pm.berg@gmail.com>
2018-02-13 10:08:42 +0100

    Added MacOS project again

---
+++ INTEGRA-7_Xamarin/INTEGRA_7_Xamarin_MacOS/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace INTEGRA_7_Xamarin_MacOS
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
