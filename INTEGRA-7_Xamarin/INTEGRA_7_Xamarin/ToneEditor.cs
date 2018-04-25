e484c5d17b76137784d791bd11830559ebe1e8d8
the51craw <35884608+the51craw@users.noreply.github.com>
2018-04-25 06:57:40 +0200

    WIP Started designing common Edit controls.

--- INTEGRA-7_Xamarin/INTEGRA_7_Xamarin/ToneEditor.cs
+++ INTEGRA-7_Xamarin/INTEGRA_7_Xamarin/ToneEditor.cs
@@ -9,46 +9,36 @@ namespace INTEGRA_7_Xamarin
 {
     public partial class UIHandler
     {
+        Grid Edit_grEditor = null;
+        Grid Edit_grHelp = null;
+        Grid Edit_grCommon = null;
+        Grid Edit_grControls = null;
+        Picker cbEditTone_PartSelector = null;
+        Picker cbEditTone_SynthesizerType = null;
+        Button tbEditTone_Instrument = null;
+        Picker cbEditTone_ParameterPages = null;
+        Picker cbEditTone_PartialSelector = null;
+        Button tbEditTone_KeyName = null;
+        Picker cbEditTone_KeySelector = null;
+        Picker cbEditTone_InstrumentCategorySelector = null;
+
         public void DrawToneEditorPage()
         {
             /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
             // Edit tone  
-            //                                                           ____________________________________ gridTones
-            //                                                          /                      ______________ gridToneData
-            //                                                         /                      /
-            // ____________________________________________________________________________________________
-            // | lvGroups             | lvCategories         | filterPresetAndUser |midiOutputDevice/part |
-            // |                      |                      |---------------------|----------------------|
-            // |                      |                      | lvToneNames         |tbSearch              |
-            // |                      |                      |                     |----------------------|
-            // |                      |                      |                     |ltToneName            |
-            // |                      |                      |                     |----------------------|
-            // |                      |                      |                     |ltType                |
-            // |                      |                      |                     |----------------------|
-            // |                      |                      |                     |ltToneNumber          |
-            // |                      |                      |                     |----------------------|
-            // |                      |                      |                     |ltBankNumber          |
-            // |                      |                      |                     |----------------------|
-            // |                      |                      |                     |ltBankMSB             |
-            // |                      |                      |                     |----------------------| Main row 0
-            // |                      |                      |                     |ltBankLSB             |
-            // |                      |                      |                     |----------------------|
-            // |                      |                      |                     |ltProgramNumber       |
-            // |                      |                      |                     |----------------------|
-            // |                      |                      |                     |Edit tone| studioset  |
-            // |                      |                      |                     |----------------------|
-            // |                      |                      |                     |Res vol  |Motion.sur. |
-            // |                      |                      |                     |----------------------|
-            // |                      |                      |                     |Favorites|Add  |Remove|
-            // |                      |                      |                     |----------------------|
-            // |                      |                      |                     |Reset hanging  |Play  |  
-            // |                      |                      |                     |----------------------|
-            // |                      |                      |                     |keyrange |+12k | -12k |
-            // |------------------------------------------------------------------------------------------|
-            // | Keyboard                                                                                 |
-            // |                                                                                          | Main row 1
-            // |                                                                                          |
-            // |__________________________________________________________________________________________|
+            // -------------------------------------------------------------------------------------------
+            // | Edit_grEdit                                        |  Edit_grHelp                       |
+            // |----------------------------------------------------|                                    |
+            // ||Edit_grCommon                                     ||                                    |
+            // ||--------------------------------------------------||                                    |
+            // ||cbPart          | cbTone         | lblPreset      ||                                    |
+            // ||--------------------------------------------------||                                    |
+            // ||cbCommon        | cbPartial      | cbToneCategory ||                                    |
+            // ||--------------------------------------------------||                                    |
+            // ||| Edit_grControls                                |||                                    |
+            // ||--------------------------------------------------||                                    |
+            // |----------------------------------------------------|                                    |
+            // -------------------------------------------------------------------------------------------
 
             // Create all controls ------------------------------------------------------------------------
 
@@ -57,6 +47,55 @@ namespace INTEGRA_7_Xamarin
             ToneEdit_NotYetImplemented.VerticalOptions = LayoutOptions.FillAndExpand;
             ToneEdit_NotYetImplemented.Text = "Not yet implemented";
             ToneEdit_NotYetImplemented.Clicked += ToneEdit_NotYetImplemented_Clicked;
+
+            Edit_grEditor = new Grid();
+            Edit_grEditor.HorizontalOptions = LayoutOptions.FillAndExpand;
+            Edit_grEditor.VerticalOptions = LayoutOptions.FillAndExpand;
+
+            Edit_grHelp = new Grid();
+            Edit_grHelp.HorizontalOptions = LayoutOptions.FillAndExpand;
+            Edit_grHelp.VerticalOptions = LayoutOptions.FillAndExpand;
+
+            Edit_grCommon = new Grid();
+            Edit_grCommon.HorizontalOptions = LayoutOptions.FillAndExpand;
+            Edit_grCommon.VerticalOptions = LayoutOptions.FillAndExpand;
+
+            Edit_grControls = new Grid();
+            Edit_grControls.HorizontalOptions = LayoutOptions.FillAndExpand;
+            Edit_grControls.VerticalOptions = LayoutOptions.FillAndExpand;
+
+            cbEditTone_PartSelector = new Picker();
+            cbEditTone_PartSelector.HorizontalOptions = LayoutOptions.FillAndExpand;
+            cbEditTone_PartSelector.VerticalOptions = LayoutOptions.FillAndExpand;
+
+            cbEditTone_SynthesizerType = new Picker();
+            cbEditTone_SynthesizerType.HorizontalOptions = LayoutOptions.FillAndExpand;
+            cbEditTone_SynthesizerType.VerticalOptions = LayoutOptions.FillAndExpand;
+
+            tbEditTone_Instrument = new Button();
+            tbEditTone_Instrument.HorizontalOptions = LayoutOptions.FillAndExpand;
+            tbEditTone_Instrument.VerticalOptions = LayoutOptions.FillAndExpand;
+
+            cbEditTone_ParameterPages = new Picker();
+            cbEditTone_ParameterPages.HorizontalOptions = LayoutOptions.FillAndExpand;
+            cbEditTone_ParameterPages.VerticalOptions = LayoutOptions.FillAndExpand;
+
+            cbEditTone_PartialSelector = new Picker();
+            cbEditTone_PartialSelector.HorizontalOptions = LayoutOptions.FillAndExpand;
+            cbEditTone_PartialSelector.VerticalOptions = LayoutOptions.FillAndExpand;
+
+            tbEditTone_KeyName = new Button();
+            tbEditTone_KeyName.HorizontalOptions = LayoutOptions.FillAndExpand;
+            tbEditTone_KeyName.VerticalOptions = LayoutOptions.FillAndExpand;
+
+            cbEditTone_KeySelector = new Picker();
+            cbEditTone_KeySelector.HorizontalOptions = LayoutOptions.FillAndExpand;
+            cbEditTone_KeySelector.VerticalOptions = LayoutOptions.FillAndExpand;
+
+            cbEditTone_InstrumentCategorySelector = new Picker();
+            cbEditTone_InstrumentCategorySelector.HorizontalOptions = LayoutOptions.FillAndExpand;
+            cbEditTone_InstrumentCategorySelector.VerticalOptions = LayoutOptions.FillAndExpand;
+
 
             // Add handlers -------------------------------------------------------------------------------
 
@@ -76,6 +115,10 @@ namespace INTEGRA_7_Xamarin
             // Assemble LibrarianStackLayout --------------------------------------------------------------
         }
 
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Tone editor handlers
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         public void ShowToneEditorPage()
         {
             if (EditorStackLayout == null)
@@ -84,5 +127,13 @@ namespace INTEGRA_7_Xamarin
             }
             mainStackLayout.Children.Add(EditorStackLayout);
         }
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Tone editor functions
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        // Tone editor helpers
+        //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
     }
 }
